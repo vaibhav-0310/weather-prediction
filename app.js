36ff168f84789e3048e12a421c4741cd69f1c2a2
vaibhav-0310 <vbhargav0310@gmail.com>
2024-07-11 22:33:10 +0530

    version 2

--- app.js
+++ app.js
@@ -7,14 +7,27 @@ let i=document.querySelectorAll("img");
 let d=document.querySelectorAll("svg");
 let country=document.querySelector(".country");
 
+
+
 btn.addEventListener("click",async()=>{
-    const url =`https://api.open-meteo.com/v1/forecast?latitude=${lat.value}&longitude=${long.value}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`;
-    const url2=`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat.value}&longitude=${long.value}&localityLanguage=en`;
+    let longitude=0;
+    let latitude=0;
+    let na="null";
+    const url3=`https://nominatim.openstreetmap.org/search?q=${lat.value},${long.value}&format=json`;
     try{
-    let loc = await fetch(url2);
-    let loca=await loc.json();
-    l.innerText=loca.locality;
-    country.innerText=`, ${loca.countryCode}.`;
+        let l=await fetch(url3);
+        let la=await l.json();
+        longitude=la[0].lon;
+        latitude=la[0].lat;
+        na=la[0].name;
+    }
+    catch(e){
+        console(e);
+    }
+    const url =`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`;
+    
+    try{
+    l.innerText=na;
     let tem=await fetch(url);
     let temp=await tem.json();
     let tt=temp.current.temperature_2m;
